f2e7fa750fc78357681b10845e2bbff06f062f9a
Janarthanan S <145856233+Janarthanan1304@users.noreply.github.com>
2025-01-27 12:10:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+
 // Display of loading screen and adjustment of body elements with respect to navigation bar, while loading the page in browser.
 $(window).on("load", function () {
-  $("body").css("padding-top", $("#navmenu").height());
+  $("body").css("padding-top", $("#navmenu").outerHeight());
   $("#outerNavbar ul li a").click(function (e) {
     e.preventDefault();
     let navigationHeight = $("#navmenu").outerHeight();
@@ -16,8 +17,7 @@ $(window)
     let navigationHeight = $("#navmenu").outerHeight();
     let scrollDistance = $(window).scrollTop();
     $(".navsection").each(function (i) {
-      console.log($(this).position().top);
-      if ($(this).position().top <= scrollDistance + navigationHeight) {
+      if ($(this).offset().top - navigationHeight <= scrollDistance) {
         $(".navbar-nav").find("li.active").removeClass("active");
         $(".navbar-nav li").eq(i).addClass("active");
       }
@@ -27,25 +27,24 @@ $(window)
 
 // Adjustment of body elements with respect to navigation bar, while resizing browser window.
 $(window).resize(function () {
-  $("body").css("padding-top", $("#navbar").height() - 53);
+  $("body").css("padding-top", $("#navmenu").outerHeight());
 });
 
 // Opening (and activating) of the relevant Skillset tab and closing all other Skillset tabs.
 function openSkill(evt, skillName) {
   let i, tabcontent, tablinks;
-
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-
-  document.getElementById(skillName).style.display = "block";
-  evt.currentTarget.className += " active";
+  let tabContentElement = document.getElementById(skillName);
+  if (tabContentElement) {
+    tabContentElement.style.display = "block";
+    evt.currentTarget.className += " active";
+  }
 }
-
 openSkill(event, 'Prog');
